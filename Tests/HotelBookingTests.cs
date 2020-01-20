fa185d9e0bde5d2617f3993a29b35f02cbe4dd10
jcopperman <opperman.jonathan@gmail.com>
2020-01-20 14:52:46 +0200

    Added Create Booking Test

--- Tests/HotelBookingTests.cs
+++ Tests/HotelBookingTests.cs
@@ -1,10 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
-namespace Automation.Web.Tests
+namespace Automation.Web
 {
-    class HotelBookingTests
+    public class Tests
     {
+        IWebDriver _driver;
+
+        public string homeUrl = "http://hotel-test.equalexperts.io/";
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver();
+
+        }
+        public void NavigatetToTheHomepage()
+        {
+            _driver.Navigate().GoToUrl(homeUrl);
+        }
+
+        public void DeleteExistingHotelBookingRecord()
+        {
+            IWebElement element = _driver.FindElement(By.Id("firstname"));
+        }
+
+        [Test]
+        public void Test_Case_003_CreateNewBookingRecord()
+        {
+            NavigatetToTheHomepage();
+
+            IWebElement firstname = _driver.FindElement(By.Id("firstname"));
+            IWebElement lastname = _driver.FindElement(By.Id("lastname"));
+            IWebElement totalprice = _driver.FindElement(By.Id("totalprice"));
+            IWebElement depositpaid = _driver.FindElement(By.Id("depositpaid"));
+            var selectIsDeposit = new SelectElement(depositpaid);
+            IWebElement checkin = _driver.FindElement(By.Id("checkin"));
+            IWebElement checkout = _driver.FindElement(By.Id("checkout"));
+            IWebElement savebutton = _driver.FindElement(By.CssSelector("#form > div:nth-child(1) > div:nth-child(7) > input:nth-child(1)"));
+
+            firstname.SendKeys("Doctor");
+            lastname.SendKeys("Who");
+            totalprice.SendKeys("1000");
+
+            //deposit
+            selectIsDeposit.SelectByText("false");
+
+            checkin.SendKeys("2020-12-25");
+            checkout.SendKeys("2020-12-25");
+
+            savebutton.Click();
+
+            //Sleep for the purpose of assessment so the assesors have time to view the tests in play
+            Thread.Sleep(3000);
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_Case_004_DeleteExistingBookingRecord()
+        {
+            //TODO: Find the row ID created by the create function to determine which delete button corresponds. 
+            NavigatetToTheHomepage();
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _driver.Close();
+        }
     }
 }
